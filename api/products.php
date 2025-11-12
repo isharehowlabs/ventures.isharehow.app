286d560ed80939e73767c3972e4b6c783c2b8a98
Jamel Eliyah <jeliyah@isharehowlabs.com>
2025-11-12 05:48:54 +0000

    working products page

---
+++ api/products.php
@@ -0,0 +1,33 @@
+<?php
+// PHP proxy for backend API
+header('Content-Type: application/json');
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
+header('Access-Control-Allow-Headers: Content-Type');
+
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit;
+}
+
+// Get query parameters
+$queryString = $_SERVER['QUERY_STRING'] ?? '';
+$backendUrl = 'http://localhost:3001/api/products' . ($queryString ? '?' . $queryString : '');
+
+// Forward request to backend
+$ch = curl_init($backendUrl);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+curl_setopt($ch, CURLOPT_TIMEOUT, 30);
+curl_setopt($ch, CURLOPT_HTTPHEADER, [
+    'Content-Type: application/json',
+]);
+
+$response = curl_exec($ch);
+$httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+curl_close($ch);
+
+http_response_code($httpCode);
+echo $response;
+?>
+
