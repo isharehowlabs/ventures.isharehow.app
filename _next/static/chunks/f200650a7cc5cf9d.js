295f03234f8935b68ab76f7d9c27bceb366ddcf2
Jamel Eliyah <jeliyah@isharehowlabs.com>
2025-11-12 04:44:14 +0000

    saving

---
+++ _next/static/chunks/f200650a7cc5cf9d.js
@@ -0,0 +1,10 @@
+__turbopack_load_page_chunks__("/products", [
+  "static/chunks/35835878b64e05ca.js",
+  "static/chunks/77e2cc9f0ffffa98.js",
+  "static/chunks/cdf36735293aedaa.js",
+  "static/chunks/e558a7a51144883e.js",
+  "static/chunks/b4d99221e9bbf65f.js",
+  "static/chunks/0a4d54729736a802.js",
+  "static/chunks/26aee19db20e0278.js",
+  "static/chunks/turbopack-8c85b6f6d2959f3b.js"
+])
