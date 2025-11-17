650d4e9690d5a5cc2bc2fc3a1a755ec01beb101c
Jamel Eliyah <jeliyah@isharehowlabs.com>
2025-11-17 23:02:50 +0000

    Build: Update production files 2025-11-17 23:02:49

---
+++ _next/static/2KO9E1LU72QWkkHbaBQgt/_buildManifest.js
@@ -0,0 +1,55 @@
+self.__BUILD_MANIFEST = {
+  "/": [
+    "static/chunks/2bcc9c154e676320.js"
+  ],
+  "/PACT": [
+    "static/chunks/d8bfddb906448762.js"
+  ],
+  "/_error": [
+    "static/chunks/b687a07db30955a9.js"
+  ],
+  "/content": [
+    "static/chunks/4df486e74bd12569.js"
+  ],
+  "/labs": [
+    "static/chunks/fc74ecf729793b01.js"
+  ],
+  "/live": [
+    "static/chunks/92406e2c20618aa2.js"
+  ],
+  "/msp": [
+    "static/chunks/cf5c8ad2c9697e31.js"
+  ],
+  "/products": [
+    "static/chunks/abf3ebc7179c8722.js"
+  ],
+  "/rise_cycling": [
+    "static/chunks/f472806da107146e.js"
+  ],
+  "/spiritual_festivals": [
+    "static/chunks/1e02b260605cb70e.js"
+  ],
+  "/wellness": [
+    "static/chunks/1196b3f2e009e3ab.js"
+  ],
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/",
+    "/PACT",
+    "/_app",
+    "/_error",
+    "/api/gemini-chat",
+    "/content",
+    "/labs",
+    "/live",
+    "/msp",
+    "/products",
+    "/rise_cycling",
+    "/spiritual_festivals",
+    "/wellness"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
