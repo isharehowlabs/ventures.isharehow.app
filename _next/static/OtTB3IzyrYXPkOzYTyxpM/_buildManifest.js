7225636633c3f976f8362856791d4cdd8d73bd74
Jamel Eliyah <jeliyah@isharehowlabs.com>
2025-11-21 07:21:22 +0000

    Build: Update production files 2025-11-21 07:21:22

---
+++ _next/static/OtTB3IzyrYXPkOzYTyxpM/_buildManifest.js
@@ -0,0 +1,55 @@
+self.__BUILD_MANIFEST = {
+  "/": [
+    "static/chunks/a1f663e8fc7a165a.js"
+  ],
+  "/PACT": [
+    "static/chunks/55e24d9c023ad8af.js"
+  ],
+  "/_error": [
+    "static/chunks/a1480c3a5d27b953.js"
+  ],
+  "/about": [
+    "static/chunks/108f8590b76c169c.js"
+  ],
+  "/content": [
+    "static/chunks/6c2dd2547f8917e8.js"
+  ],
+  "/labs": [
+    "static/chunks/95eec2c9ad999f41.js"
+  ],
+  "/live": [
+    "static/chunks/cf4075f64688e270.js"
+  ],
+  "/products": [
+    "static/chunks/7df22fd35c6192a8.js"
+  ],
+  "/profile": [
+    "static/chunks/8308d1d1c9de309b.js"
+  ],
+  "/rise": [
+    "static/chunks/d250fe72e125de3f.js"
+  ],
+  "/settings": [
+    "static/chunks/a3069a49889997ff.js"
+  ],
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/",
+    "/PACT",
+    "/_app",
+    "/_error",
+    "/about",
+    "/api/gemini-chat",
+    "/content",
+    "/labs",
+    "/live",
+    "/products",
+    "/profile",
+    "/rise",
+    "/settings"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
---
+++ _next/static/chunks/6c2dd2547f8917e8.js
@@ -0,0 +1,12 @@
+__turbopack_load_page_chunks__("/content", [
+  "static/chunks/18568a00d9c8f61f.js",
+  "static/chunks/bbb7468d246fb9a9.js",
+  "static/chunks/1440369c878b6374.js",
+  "static/chunks/735e31ae1575beb5.js",
+  "static/chunks/2f7d47752deac485.js",
+  "static/chunks/1e1fcc56f2b761d9.js",
+  "static/chunks/2d49f87e7c413d4b.js",
+  "static/chunks/32ebd76a01eb664b.js",
+  "static/chunks/e468971601653d0f.js",
+  "static/chunks/turbopack-5bc04d9c3cb618f3.js"
+])
