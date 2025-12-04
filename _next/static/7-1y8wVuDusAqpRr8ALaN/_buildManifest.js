4a59fd8fea84c3537cbfd2a04d4c33f71927b159
Jamel Eliyah <jeliyah@isharehowlabs.com>
2025-12-04 22:36:55 +0000

    Build: Update production files 2025-12-04 22:36:54

---
+++ _next/static/7-1y8wVuDusAqpRr8ALaN/_buildManifest.js
@@ -0,0 +1,86 @@
+self.__BUILD_MANIFEST = {
+  "/": [
+    "static/chunks/ae3bb360c85f92b6.js"
+  ],
+  "/PACT": [
+    "static/chunks/fb3f6ed9d063fb6b.js"
+  ],
+  "/_error": [
+    "static/chunks/96f48b7f04f4ef8a.js"
+  ],
+  "/board": [
+    "static/chunks/ba615ef40f1825e2.js"
+  ],
+  "/content": [
+    "static/chunks/8f700bf1c84809cb.js"
+  ],
+  "/cowork": [
+    "static/chunks/7fc4de84c32ca8b9.js"
+  ],
+  "/creative": [
+    "static/chunks/37ad9106ca67afbe.js"
+  ],
+  "/demo": [
+    "static/chunks/997623d7069d9ccd.js"
+  ],
+  "/labs": [
+    "static/chunks/f7e4d0ede4250eaa.js"
+  ],
+  "/live": [
+    "static/chunks/618254ecd92538a6.js"
+  ],
+  "/lookupcafe": [
+    "static/chunks/f5b233e882b3197d.js"
+  ],
+  "/privacy": [
+    "static/chunks/256eaf07c010bac7.js"
+  ],
+  "/products": [
+    "static/chunks/fb6d403cf59d255c.js"
+  ],
+  "/profile": [
+    "static/chunks/a2ea47555772da85.js"
+  ],
+  "/prospecting": [
+    "static/chunks/388eba4424ccc6d0.js"
+  ],
+  "/rise": [
+    "static/chunks/d62e88f12f1db637.js"
+  ],
+  "/settings": [
+    "static/chunks/1f3c790386a4c43b.js"
+  ],
+  "/terms": [
+    "static/chunks/75c20a949ef5f5d2.js"
+  ],
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/",
+    "/PACT",
+    "/_app",
+    "/_error",
+    "/api/gemini-chat",
+    "/api/pricing/tiers",
+    "/api/subscriptions/create",
+    "/api/subscriptions/current",
+    "/board",
+    "/content",
+    "/cowork",
+    "/creative",
+    "/demo",
+    "/labs",
+    "/live",
+    "/lookupcafe",
+    "/privacy",
+    "/products",
+    "/profile",
+    "/prospecting",
+    "/rise",
+    "/settings",
+    "/terms"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
