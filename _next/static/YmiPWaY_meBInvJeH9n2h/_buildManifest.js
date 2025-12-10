8ec6b7af8e489dafbad4cbb1d76c9e3ed171fe5a
Jamel Eliyah <jeliyah@isharehowlabs.com>
2025-12-10 02:50:08 +0000

    Build: Update production files 2025-12-10 02:50:07

---
+++ _next/static/YmiPWaY_meBInvJeH9n2h/_buildManifest.js
@@ -0,0 +1,102 @@
+self.__BUILD_MANIFEST = {
+  "/": [
+    "static/chunks/7dacb5af2f4fbd51.js"
+  ],
+  "/_error": [
+    "static/chunks/da54460d37f82bba.js"
+  ],
+  "/blog": [
+    "static/chunks/54e777f6de5da27e.js"
+  ],
+  "/blog/[slug]": [
+    "static/chunks/b8a1ebd53a9a8ffd.js"
+  ],
+  "/board": [
+    "static/chunks/e4a0bf26066b9b19.js"
+  ],
+  "/content": [
+    "static/chunks/20b1f40ec19e7fb5.js"
+  ],
+  "/creative": [
+    "static/chunks/e8fcaae440da4564.js"
+  ],
+  "/creative-services": [
+    "static/chunks/1f17a450d97d135b.js"
+  ],
+  "/demo": [
+    "static/chunks/96145602e5f5fb1a.js"
+  ],
+  "/growth-machine": [
+    "static/chunks/ac184733fd7c3451.js"
+  ],
+  "/learning-hub": [
+    "static/chunks/b24c6541c23d58ee.js"
+  ],
+  "/link-wallet": [
+    "static/chunks/f5edf8db5eb4c0ed.js"
+  ],
+  "/live": [
+    "static/chunks/1f11314821d776e3.js"
+  ],
+  "/lookupcafe": [
+    "static/chunks/85d975e04b3093dc.js"
+  ],
+  "/portfolio": [
+    "static/chunks/ceb20b5da9329bd0.js"
+  ],
+  "/privacy": [
+    "static/chunks/c01298779baa940e.js"
+  ],
+  "/products": [
+    "static/chunks/5724573a119eab88.js"
+  ],
+  "/profile": [
+    "static/chunks/165d48db00f586ed.js"
+  ],
+  "/prospecting": [
+    "static/chunks/c9f0c5e5cad3dfd6.js"
+  ],
+  "/rise": [
+    "static/chunks/9ae519c9f9ca6db3.js"
+  ],
+  "/settings": [
+    "static/chunks/9b679ccb9ac83814.js"
+  ],
+  "/terms": [
+    "static/chunks/95baf23cf515face.js"
+  ],
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/",
+    "/_app",
+    "/_error",
+    "/api/gemini-chat",
+    "/api/pricing/tiers",
+    "/api/subscriptions/create",
+    "/api/subscriptions/current",
+    "/blog",
+    "/blog/[slug]",
+    "/board",
+    "/content",
+    "/creative",
+    "/creative-services",
+    "/demo",
+    "/growth-machine",
+    "/learning-hub",
+    "/link-wallet",
+    "/live",
+    "/lookupcafe",
+    "/portfolio",
+    "/privacy",
+    "/products",
+    "/profile",
+    "/prospecting",
+    "/rise",
+    "/settings",
+    "/terms"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
